285823b1e72574df012b71a2a0ccac5b460803dc
suwonthugger <ddww1029wwdd@gmail.com>
2024-07-04 00:30:28 +0900

    feat: 리액트 쿼리 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { RouterProvider } from 'react-router-dom';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { router } from './Router';
 
+const queryClient = new QueryClient();
+
 const App = () => {
-	return <RouterProvider router={router} />;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	);
 };
 
 export default App;
