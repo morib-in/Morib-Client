15b7dc9f82a0edcd3c42c3370c5d77ee9d7da232
ivoryeee <psa109@sookmyung.ac.kr>
2024-07-04 18:46:11 +0900

    Feat: tailwind theme 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,41 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss')'Config} */
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				pretendard: ['Pretendard'],
+			},
+			colors: {
+				'mint-01': '#06FFD2',
+				'mint-02': '#8BFFEA',
+				'mint-02-hover': '#7DE6D3',
+				'mint-02-press': '#6FCCBB',
+				secondary: '#00BF9C',
+				'date-active': 'rgba(139, 255, 234, 0.1)',
+				'gray-bg-01': '#181C22',
+				'gray-bg-02': '#292D34',
+				'gray-bg-03': '#33373F',
+				'gray-bg-04': '#414751',
+				'gray-bg-05': '#4F5762',
+				'gray-bg-06': '#656D77',
+				'gray-bg-07': '#656D77',
+				'gray-01': '#333333',
+				'gray-02': '#5D5D5D',
+				'gray-03': '#7C7C7C',
+				'gray-04': '#A5A5A5',
+				'gray-05': '#DFDFDF',
+				white: '#FFFFFF',
+				'error-01': '#F84949',
+				'error-02': '##FF1717',
+				black: '#000000',
+				dim: 'rgba(0, 0, 0, 0.6)',
+			},
+			lineHeight: {
+				120: '120%',
+				140: '140%',
+			},
+		},
 	},
 	plugins: [],
 };
