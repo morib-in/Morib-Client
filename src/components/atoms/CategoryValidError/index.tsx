0ac591bb4703222f142f5f7042861e6077aeb9ff
ivoryeee <psa109@sookmyung.ac.kr>
2024-07-08 03:12:26 +0900

    Fix: 아이콘 네이밍 수정

--- src/components/atoms/CategoryValidError/index.tsx
+++ src/components/atoms/CategoryValidError/index.tsx
@@ -1,9 +1,9 @@
-import Ic_alert from '@/assets/svgs/ic_alert.svg';
+import AlertIcon from '@/assets/svgs/AlertIcon.svg?react';
 
 const CategoryValidError = () => {
 	return (
 		<div className="my-[0.6rem] flex">
-			<img src={Ic_alert} alt="이메일 유효성 검사 실패" />
+			<AlertIcon />
 			<div className="detail-reg-14 ml-[0.5rem] text-error-01">알맞은 도메인을 입력해주세요.</div>
 		</div>
 	);
