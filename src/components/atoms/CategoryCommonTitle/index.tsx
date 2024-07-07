795eda7f37c7b6de24db4060b68360eb8ac075f2
hanseo <yds06206@gmail.com>
2024-07-07 20:35:16 +0900

    style: 카테고리 모달 공통 컴포넌트 타이틀 구현

---
+++ src/components/atoms/CategoryCommonTitle/index.tsx
@@ -0,0 +1,11 @@
+const CategoryCommonTitle = () => {
+	return (
+		<>
+			<header className="mx-[4.4rem] mt-[2.8rem]">
+				<h1 className="head-bold-24 text-gray-04">카테고리 추가</h1>
+			</header>
+		</>
+	);
+};
+
+export default CategoryCommonTitle;
