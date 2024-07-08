2972c6c51d4534285bc58bfc647412d8a82ec058
hanseo <yds06206@gmail.com>
2024-07-08 18:07:32 +0900

    code review: 코드리뷰 반영

--- src/components/atoms/CategoryCommonBtn/index.tsx
+++ src/components/atoms/CategoryCommonBtn/index.tsx
@@ -1,12 +1,12 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react';
 
-export interface CategoryBtnProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface CategoryBtnProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	variant?: '취소' | '완료';
 	isDisabled?: boolean;
 	children?: ReactNode;
 }
 
-const CategoryCommonBtn = ({ variant, isDisabled, ...props }: CategoryBtnProps) => {
+const CategoryCommonBtn = ({ variant, isDisabled, children, ...props }: CategoryBtnProps) => {
 	const btnVariant = {
 		취소: 'text-white bg-gray-bg-06',
 		완료: 'text-gray-bg-01 bg-mint-02',
@@ -15,14 +15,12 @@ const CategoryCommonBtn = ({ variant, isDisabled, ...props }: CategoryBtnProps) 
 
 	const commonStyle = ' px-[4.8rem] py-[1rem] rounded-[5px] subhead-semibold-18';
 
-	const styledBtn = variant === '취소' ? btnVariant.취소 : isDisabled ? disabledBtn : btnVariant.완료;
+	const styledBtn = isDisabled ? disabledBtn : variant ? btnVariant[variant] : '';
 
 	return (
-		<>
-			<button className={styledBtn + commonStyle} disabled={isDisabled} {...props}>
-				{variant}
-			</button>
-		</>
+		<button className={styledBtn + commonStyle} {...props}>
+			{children}
+		</button>
 	);
 };
 
