ff0b70aafde6ffe42484559a7e8704af58710bdb
ivoryeee <psa109@sookmyung.ac.kr>
2024-07-09 02:42:56 +0900

    Fix: Input창 padding style 수정

--- src/components/atoms/CategoryInputTitle/index.tsx
+++ src/components/atoms/CategoryInputTitle/index.tsx
@@ -1,9 +1,9 @@
-interface CategoryInputTitleType {
+interface CategoryInputTitleProps {
 	title: string;
 }
 
-const CategoryInputTitle = ({ title }: CategoryInputTitleType) => {
-	return <div className="subhead-bold-22 pb-[1rem] pl-[1rem] text-white">{title}</div>;
+const CategoryInputTitle = ({ title }: CategoryInputTitleProps) => {
+	return <div className="subhead-bold-22 pb-[1rem] text-white">{title}</div>;
 };
 
 export default CategoryInputTitle;
