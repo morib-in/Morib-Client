132ebdb9e03ff6b8680eb2c4944941c481a03e3b
hanseo <yds06206@gmail.com>
2024-07-09 00:17:40 +0900

    code review: 버튼 비활성화 기능 추가

--- src/components/atoms/CategoryCommonBtn/index.tsx
+++ src/components/atoms/CategoryCommonBtn/index.tsx
@@ -2,11 +2,11 @@ import { ButtonHTMLAttributes, ReactNode } from 'react';
 
 interface CategoryBtnProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	variant?: '취소' | '완료';
-	isDisabled?: boolean;
+
 	children?: ReactNode;
 }
 
-const CategoryCommonBtn = ({ variant, isDisabled, children, ...props }: CategoryBtnProps) => {
+const CategoryCommonBtn = ({ disabled, variant, children, ...props }: CategoryBtnProps) => {
 	const btnVariant = {
 		취소: 'text-white bg-gray-bg-06',
 		완료: 'text-gray-bg-01 bg-mint-02',
@@ -15,10 +15,10 @@ const CategoryCommonBtn = ({ variant, isDisabled, children, ...props }: Category
 
 	const commonStyle = ' px-[4.8rem] py-[1rem] rounded-[5px] subhead-semibold-18';
 
-	const styledBtn = isDisabled ? disabledBtn : variant ? btnVariant[variant] : '';
+	const styledBtn = disabled ? disabledBtn : variant ? btnVariant[variant] : '';
 
 	return (
-		<button className={styledBtn + commonStyle} {...props}>
+		<button className={`${styledBtn} ${commonStyle}`} disabled={disabled} {...props}>
 			{children}
 		</button>
 	);
