c8390af77e86bff1081e57d11b626c05a03dd4cc
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-07-09 22:59:00 +0900

    style: jpg 이미지 png 이미지로 변경

--- src/mocks/userFriendData.ts
+++ src/mocks/userFriendData.ts
@@ -1,8 +1,7 @@
-import img1 from '@/assets/images/profile_image1.jpg';
-import img2 from '@/assets/images/profile_image2.jpg';
-import img3 from '@/assets/images/profile_image3.jpg';
-import img4 from '@/assets/images/profile_image4.jpg';
-import img5 from '@/assets/images/profile_image5.jpg';
+import img1 from '@/assets/images/profile_image1.png';
+import img2 from '@/assets/images/profile_image2.png';
+import img3 from '@/assets/images/profile_image3.png';
+import img4 from '@/assets/images/profile_image4.png';
 
 export const userFriendData = [
 	{
@@ -25,13 +24,13 @@ export const userFriendData = [
 	},
 	{
 		id: 3,
-		image: img4,
+		image: img1,
 		name: '김온유',
 		categoryname: 'IA',
 	},
 	{
 		id: 4,
-		image: img5,
+		image: img4,
 		name: '강서현',
 		categoryname: '앱잼 와이어프...',
 	},
