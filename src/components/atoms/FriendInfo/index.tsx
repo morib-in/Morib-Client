e00f8c2dc4ac9c4f0f8fa66aef6542220470c51e
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-07-10 03:31:24 +0900

    feat: time props 추가

--- src/components/atoms/FriendInfo/index.tsx
+++ src/components/atoms/FriendInfo/index.tsx
@@ -3,16 +3,22 @@ import ClockIcon from '@/assets/svgs/icon_clock.svg?react';
 interface UserFriendDataProps {
 	image: string;
 	name: string;
+	time: number;
 	categoryname: string;
 }
 
-const FriendInfo = ({ image, name, categoryname }: UserFriendDataProps) => {
+const FriendInfo = ({ image, name, time, categoryname }: UserFriendDataProps) => {
+	const formatTime = (unit: number) => unit.toString().padStart(2, '0');
+	const hours = Math.floor(time / 3600);
+	const minutes = Math.floor((time % 3600) / 60);
+	const seconds = time % 60;
+
 	return (
-		<div className="flex h-[13.8rem] w-[8.2rem] flex-col items-center justify-center">
-			<img src={image} alt="유저 프로필" className="h-[7.4rem] w-[7.4rem]" />
+		<div className="py-[0.5rem 0.7rem] flex h-[15rem] w-[9.8rem] flex-col items-center justify-center px-[0.8rem]">
+			<img src={image} alt="유저 프로필" className="mb-[1rem] h-[7.4rem] w-[7.4rem]" />
 			<div className="flex items-center justify-center">
 				<ClockIcon />
-				<span className="detail-reg-14 text-mint-01" />
+				<span className="detail-reg-14 text-mint-01">{`${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`}</span>
 			</div>
 			<span className="detail-semibold-14 text-white">{name}</span>
 			<span className="detail-reg-12 text-gray-04">{categoryname}</span>
--- src/components/molecules/FriendInfoCarousel/index.tsx
+++ src/components/molecules/FriendInfoCarousel/index.tsx
@@ -6,7 +6,13 @@ const FriendInfoCarousel = () => {
 	return (
 		<div className="flex h-screen w-full flex-wrap items-center justify-center gap-[4.2rem] bg-gray-bg-01">
 			{userFriendData.map((friend: any) => (
-				<FriendInfo key={friend.id} image={friend.image} name={friend.name} categoryname={friend.categoryname} />
+				<FriendInfo
+					key={friend.id}
+					image={friend.image}
+					time={friend.time}
+					name={friend.name}
+					categoryname={friend.categoryname}
+				/>
 			))}
 		</div>
 	);
