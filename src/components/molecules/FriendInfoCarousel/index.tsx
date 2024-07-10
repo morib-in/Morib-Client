cecbcaf499159c791490794e74dbddec76de22e1
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-07-10 16:18:45 +0900

    style: 최상단 div 너비, 높이 조정

--- src/components/molecules/FriendInfoCarousel/index.tsx
+++ src/components/molecules/FriendInfoCarousel/index.tsx
@@ -4,7 +4,7 @@ import { userFriendData } from '@/mocks/userFriendData';
 
 const FriendInfoCarousel = () => {
 	return (
-		<div className="flex h-screen w-full flex-wrap items-center justify-center gap-[4.2rem] bg-gray-bg-01">
+		<div className="flex h-[15rem] w-[86.2rem] flex-wrap items-center justify-center gap-[4.2rem] bg-gray-bg-01">
 			{userFriendData.map((friend: any) => (
 				<FriendInfo
 					key={friend.id}
