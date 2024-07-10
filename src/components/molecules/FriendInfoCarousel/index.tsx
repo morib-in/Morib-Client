3190e88a1905658e208a58a4105eaf6a895fbae9
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-07-10 23:19:17 +0900

    feat: 캐러셀 구현 완료

--- src/components/molecules/FriendInfoCarousel/index.tsx
+++ src/components/molecules/FriendInfoCarousel/index.tsx
@@ -1,24 +1,28 @@
 import ArrowSVGBtn from '@/components/atoms/ArrowSVGBtn';
 import FriendInfo from '@/components/atoms/FriendInfo';
 
+import useFriendInfoCarousel from '@/hooks/useFriendInfoCarousel';
+
 import { Direction } from '@/types/global';
 
-import { userFriendData } from '@/mocks/userFriendData';
-
 const FriendInfoCarousel = () => {
+	const { handlePrevClick, handleNextClick, visibleFriends } = useFriendInfoCarousel();
+
 	return (
-		<div className="flex h-[15rem] w-[86.2rem] flex-wrap items-center justify-center gap-[4.2rem] bg-gray-bg-01">
-			<ArrowSVGBtn className="mr-[1.6rem]" direction={Direction.LEFT} />
-			{userFriendData.map((friend: any) => (
-				<FriendInfo
-					key={friend.id}
-					image={friend.image}
-					time={friend.time}
-					name={friend.name}
-					categoryname={friend.categoryname}
-				/>
-			))}
-			<ArrowSVGBtn className="ml-[1.6rem]" direction={Direction.RIGHT} />
+		<div className="flex h-[15rem] w-[86.2rem] items-center justify-center bg-gray-bg-01">
+			<ArrowSVGBtn className="mr-[1.6rem]" direction={Direction.LEFT} onClick={handlePrevClick} />
+			<div className="flex gap-[4.2rem]">
+				{visibleFriends().map((friend) => (
+					<FriendInfo
+						key={friend.id}
+						image={friend.image}
+						time={friend.time}
+						name={friend.name}
+						categoryname={friend.categoryname}
+					/>
+				))}
+			</div>
+			<ArrowSVGBtn className="ml-[1.6rem]" direction={Direction.RIGHT} onClick={handleNextClick} />
 		</div>
 	);
 };
