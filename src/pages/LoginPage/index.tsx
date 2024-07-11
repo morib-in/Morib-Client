510e6f3ba16ba80f682ef3f03bf90a1236c87529
suwonthugger <ddww1029wwdd@gmail.com>
2024-07-11 23:04:57 +0900

    chore: 불필요한 코드 정리

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,15 +1,5 @@
-import TodayTodoSmallBtn from '@/components/atoms/TodayTodoSmallBtn';
-import TodayTodoBox from '@/components/molecules/TodayTodoBox';
-
-import { todoData } from '@/mocks/homeData';
-
 const LoginPage = () => {
-	return (
-		<>
-			Login Page <TodayTodoSmallBtn>취소</TodayTodoSmallBtn>
-			<TodayTodoBox time={2000} selectedTodayTodos={todoData} todos={todoData} />
-		</>
-	);
+	return <>Login Page</>;
 };
 
 export default LoginPage;
