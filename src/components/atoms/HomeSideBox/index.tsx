ecca438620885937b29974b36f14e04acc54f90c
suwonthugger <ddww1029wwdd@gmail.com>
2024-07-12 20:02:00 +0900

    style: HomeSideBar 구현

---
+++ src/components/atoms/HomeSideBox/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+
+interface HomeSideBoxProps {
+	children: ReactNode;
+}
+
+const HomeSideBox = ({ children }: HomeSideBoxProps) => {
+	return (
+		<div className="flex h-screen w-[7.4rem] flex-col items-center justify-between bg-gray-bg-02 pb-[2.1rem] pt-[5.4rem]">
+			{children}
+		</div>
+	);
+};
+
+export default HomeSideBox;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,11 @@
+import HomeSideBar from '@/components/molecules/HomeSideBar';
+
 const HomePage = () => {
-	return <div className="flex items-center justify-center">Home</div>;
+	return (
+		<div className="flex items-center justify-center">
+			<HomeSideBar />
+		</div>
+	);
 };
 
 export default HomePage;
