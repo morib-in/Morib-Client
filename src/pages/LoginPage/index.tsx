4e580e319a4b49bac8701d14a9e7eae16c5eb026
hanseo <yds06206@gmail.com>
2024-07-14 18:47:27 +0900

    remove: 필요없는 코드 삭제

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,19 +1,4 @@
-import { useRef, useState } from 'react';
-
-import AddCategoryListModal from '@/components/templates/Button/AddCategoryListModal';
-
 const LoginPage = () => {
-	const [isOpen, setOpen] = useState(false);
-	const dialogRef = useRef<HTMLDialogElement>(null);
-	const showModal = () => {
-		dialogRef.current?.showModal();
-		setOpen(true);
-	};
-	return (
-		<>
-			<button onClick={showModal}>모달 열기</button>
-			<AddCategoryListModal dialogRef={dialogRef} />
-		</>
-	);
+	return <>Login Page</>;
 };
 export default LoginPage;
