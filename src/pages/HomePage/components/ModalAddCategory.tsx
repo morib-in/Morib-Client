1f12fbe0301d3e60ceeaf1dbb6892258e9a7557f
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-08-19 02:05:30 +0900

    fix: ModalAddCategory 파일 import 오류 수정 (#180)

--- src/pages/HomePage/components/ModalAddCategory.tsx
+++ src/pages/HomePage/components/ModalAddCategory.tsx
@@ -3,22 +3,13 @@ import { useEffect, useRef, useState } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 
 import ButtonCategoryCommon from '@/shared/components/ButtonCategoryCommon';
+import Calendar from '@/shared/components/Calendar';
 import TitleCategoryCommon from '@/shared/components/TitleCategoryCommon';
 
 import { getTabName } from '@/shared/apis/tasks/axios/index';
 import { useGetTabName, usePostCategory } from '@/shared/apis/tasks/queries/index';
 
 import { formatCalendarApiDate } from '@/shared/utils/calendar/index';
-
-import CalendarInput from '@/components/atoms/CalendarInput/index';
-import CategoryInputTitle from '@/components/atoms/CategoryInputTitle/index';
-import CategoryMoribContentPage from '@/components/atoms/CategoryMoribContentPage';
-import CategoryMoribContentUrl from '@/components/atoms/CategoryMoribContentUrl';
-import CategoryToggle from '@/components/atoms/CategoryToggle/index';
-import Calendar from '@/components/molecules/Calendar/index';
-import CategoryInputMoribName from '@/components/molecules/CategoryInputMoribName/index';
-import CategoryMoribContentSet from '@/components/molecules/CategoryMoribContentSet';
-import CategoryMoribSet from '@/components/molecules/CategoryMoribSet';
 
 interface UrlInfo {
 	url: string;
