52deca948511fc237e1ae9b06b0c4a49057ad642
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-08-21 02:35:10 +0900

    feat: errorBoundary 구현 (#181)

---
+++ src/shared/components/ErrorBoundary/ApiErrorBoundary.tsx
@@ -0,0 +1,88 @@
+import { Component, ComponentType, ReactNode } from 'react';
+
+import { AxiosError } from 'axios';
+
+import { SHOULD_HANDLE_ERROR } from '@/shared/constants/error';
+
+interface FallbackProps {
+	error?: AxiosError;
+	resetError?: () => void;
+}
+
+interface ApiErrorBoundaryProps {
+	children: ReactNode;
+	fallback: ComponentType<FallbackProps>;
+	handleError?: () => void;
+}
+
+interface ApiErrorBoundaryState {
+	shouldHandleError: boolean;
+	shouldRethrow: boolean;
+	error: AxiosError | Error | null;
+}
+
+class ApiErrorBoundary extends Component<ApiErrorBoundaryProps, ApiErrorBoundaryState> {
+	constructor(props: ApiErrorBoundaryProps) {
+		super(props);
+		this.state = {
+			shouldHandleError: false,
+			shouldRethrow: false,
+			error: null,
+		};
+	}
+
+	static getDerivedStateFromError(error: AxiosError | Error): ApiErrorBoundaryState {
+		// 에러를 특정 API 에러로 가정하고 처리할 수 있는지 확인
+		if (
+			error instanceof AxiosError &&
+			error?.response?.status &&
+			SHOULD_HANDLE_ERROR.includes(error?.response?.status)
+		) {
+			return {
+				shouldHandleError: true,
+				shouldRethrow: false,
+				error,
+			};
+		}
+
+		// 처리할 수 없는 에러는 상위 에러 바운더리로 전달
+		return {
+			shouldHandleError: false,
+			shouldRethrow: true,
+			error,
+		};
+	}
+
+	// retry
+	resetError = () => {
+		this.props.handleError?.();
+
+		this.setState({
+			shouldHandleError: false,
+			shouldRethrow: false,
+			error: null,
+		});
+	};
+
+	render() {
+		const { shouldHandleError, shouldRethrow, error } = this.state;
+		const { fallback: Fallback } = this.props;
+
+		if (shouldRethrow && error) {
+			throw error; // 상위 Error Boundary로 에러를 전달
+		}
+
+		if (!shouldHandleError) {
+			return this.props.children; // 에러가 처리 대상이 아니면 원래의 UI를 그대로 렌더링
+		}
+
+		// 에러에 따라 UI를 분기 처리
+		if (error instanceof AxiosError) {
+			return <Fallback error={error} resetError={this.resetError} />;
+		}
+
+		return null;
+	}
+}
+
+export default ApiErrorBoundary;
---
+++ src/shared/components/ErrorBoundary/GlobalErrorBoundary.tsx
@@ -0,0 +1,70 @@
+import { Component, ReactNode } from 'react';
+
+import { AxiosError } from 'axios';
+
+import { ERROR_CODES } from '@/shared/constants/error';
+
+interface GlobalErrorBoundaryProps {
+	children: ReactNode;
+}
+
+interface GlobalErrorBoundaryState {
+	shouldHandleError: boolean;
+	error: Error | AxiosError | null;
+}
+
+class GlobalErrorBoundary extends Component<GlobalErrorBoundaryProps, GlobalErrorBoundaryState> {
+	constructor(props: GlobalErrorBoundaryProps) {
+		super(props);
+		this.state = {
+			shouldHandleError: false,
+			error: null,
+		};
+	}
+
+	static getDerivedStateFromError(error: Error): GlobalErrorBoundaryState {
+		// 에러를 캐치하고 상태를 업데이트
+		return {
+			shouldHandleError: true,
+			error,
+		};
+	}
+
+	resetError = () => {
+		this.setState({
+			shouldHandleError: false,
+			error: null,
+		});
+	};
+
+	render() {
+		const { shouldHandleError, error } = this.state;
+
+		if (!shouldHandleError) {
+			return this.props.children;
+		}
+
+		// // 네트워크 에러 처리
+		// if (
+		// 	error instanceof AxiosError &&
+		// 	error.response?.status &&
+		// 	error.request.status === ERROR_CODES.INTERNAL_SERVER_ERROR
+		// ) {
+		// 	return <NetworkError onClickRetry={this.resetError} />;
+		// }
+
+		// // 서버 점검 에러 처리
+		// if (
+		// 	error instanceof AxiosError &&
+		// 	error.response?.status &&
+		// 	error.request.status === ERROR_CODES.SERVICE_UNAVAILABLE
+		// ) {
+		// 	return <Maintenance />;
+		// }
+
+		// // 알 수 없는 에러 처리
+		// return <UnknownError onClickRetry={this.resetError} />;
+	}
+}
+
+export default GlobalErrorBoundary;
