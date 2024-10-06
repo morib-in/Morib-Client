3f6e0578589709a9137d9c0ba60b59402189ed79
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-10-07 03:03:44 +0900

    feat: dropdown 공통 컴포넌트 구현 (#201)

---
+++ src/shared/components/Dropdown.tsx
@@ -0,0 +1,111 @@
+import { ButtonHTMLAttributes, ReactNode, createContext, useContext, useRef, useState } from 'react';
+
+import useClickOutside from '../hooks/useClickOutside';
+
+interface DropdownContextProps {
+	open: boolean;
+	handleToggleOpen: () => void;
+	handleToggleClose: () => void;
+}
+
+const DropdownContext = createContext({});
+
+// useDropdownContext: Select 컴포넌트 외부에서 서브 컴포넌트들이 사용됐을 때 에러 처리
+function useDropdownContext() {
+	const context = useContext(DropdownContext);
+	if (Object.keys(context).length === 0) {
+		throw new Error('Select 컴포넌트는 Select.Root 내에서 사용되어야 합니다.');
+	}
+	return context as DropdownContextProps;
+}
+
+// Dropdown root 컴포넌트
+interface DropdownRootProps {
+	children: ReactNode;
+}
+const DropdownRoot = ({ children }: DropdownRootProps) => {
+	const [open, setOpen] = useState(false);
+
+	const handleToggleOpen = () => setOpen((prev) => !prev);
+	const handleClose = () => setOpen(false);
+
+	const ref = useRef(null);
+	useClickOutside(ref, handleClose);
+
+	const contextValue: DropdownContextProps = {
+		open,
+		handleToggleOpen,
+		handleToggleClose: handleClose,
+	};
+
+	return (
+		<DropdownContext.Provider value={contextValue}>
+			<div ref={ref}>{children}</div>
+		</DropdownContext.Provider>
+	);
+};
+
+// Dropdown의 메뉴를 trigger 하는 컴포넌트
+interface DropdownTriggerProps {
+	children: ReactNode;
+}
+
+const DropdownTrigger = ({ children }: DropdownTriggerProps) => {
+	const { handleToggleOpen } = useDropdownContext();
+
+	return (
+		<button onClick={handleToggleOpen} className="relative">
+			{children}
+		</button>
+	);
+};
+
+// Dropdown의 메뉴 리스트를 렌더링하는 컴포넌트
+interface DropdownContentProps {
+	children: ReactNode;
+	maxHeight?: string; // dropdown 메뉴의 최대 높이를 지정해서 스크롤 할 수 있음
+	boxShadow?: string;
+}
+
+const DropdownContent = ({ children, maxHeight, boxShadow }: DropdownContentProps) => {
+	const { open, handleToggleClose } = useDropdownContext();
+
+	const shadowStyle = boxShadow ? boxShadow : 'shadow-[0_3px_30px_0_rgba(0,0,0,0.4)]';
+
+	return (
+		<ul onClick={handleToggleClose} className={`absolute overflow-scroll rounded-[4px] ${shadowStyle} ${maxHeight}`}>
+			{open && children}
+		</ul>
+	);
+};
+
+// Dropdown의 메뉴 리스트 아이템 컴포넌트
+interface DropdownItemProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	label: string;
+	textColor?: 'default' | 'red';
+}
+
+const DropdownItem = ({ label, textColor = 'default', onClick, ...props }: DropdownItemProps) => {
+	const textStyle = textColor === 'red' ? 'text-error-01' : 'text-white';
+
+	return (
+		<li className="border-t border-t-gray-bg-04 first:border-none">
+			<button
+				onClick={onClick}
+				{...props}
+				className={`flex bg-gray-bg-02 px-[1.6rem] py-[0.4rem] hover:bg-gray-bg-03 active:bg-gray-bg-04`}
+			>
+				<p className={`detail-reg-12 flex h-[2.2rem] min-w-[13.5rem] items-center ${textStyle}`}>{label}</p>
+			</button>
+		</li>
+	);
+};
+
+const Dropdown = {
+	Root: DropdownRoot,
+	Trigger: DropdownTrigger,
+	Content: DropdownContent,
+	Item: DropdownItem,
+};
+
+export default Dropdown;
