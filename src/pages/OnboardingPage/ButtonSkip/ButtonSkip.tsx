f83dd2b83dd69077e0c76001a3aa659bb73a8043
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-03-06 19:17:13 +0900

    feat: 스킵 버튼 컴포넌트 추가

---
+++ src/pages/OnboardingPage/ButtonSkip/ButtonSkip.tsx
@@ -0,0 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
+import { ROUTES_CONFIG } from '@/router/routesConfig';
+
+const ButtonSkip = () => {
+	const navigate = useNavigate();
+	const handleNavigateToHome = () => {
+		navigate(ROUTES_CONFIG.home.path);
+	};
+	return (
+		<button onClick={handleNavigateToHome} className="text-gray-04 underline underline-offset-[0.6rem] subhead-reg-22">
+			건너뛰기
+		</button>
+	);
+};
+
+export default ButtonSkip;
