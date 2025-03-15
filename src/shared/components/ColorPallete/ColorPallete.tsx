1a061928ef0a0a15db91070e70c05a268b9f3f58
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-03-15 16:39:28 +0900

    fix: 컬레파레트 선택 시 border 추가될 수 있게 하는 기능 및 위치 className으로 받아서 조정할 수 있게 수정

--- src/shared/components/ColorPallete/ColorPallete.tsx
+++ src/shared/components/ColorPallete/ColorPallete.tsx
@@ -4,11 +4,12 @@ import { COLOR_PALETTE_MAP } from '@/shared/constants/colorPalette';
 
 interface ColorPaletteRootProps {
 	isOpen: boolean;
+	className?: string;
 	children: ReactNode;
 }
 
 const ColorPaletteRoot = forwardRef<HTMLDivElement, ColorPaletteRootProps>(function ColorPaletteRoot(
-	{ isOpen, children },
+	{ isOpen, className, children },
 	ref,
 ) {
 	if (!isOpen) return null;
@@ -16,7 +17,7 @@ const ColorPaletteRoot = forwardRef<HTMLDivElement, ColorPaletteRootProps>(funct
 	return (
 		<div
 			ref={ref}
-			className="absolute left-0 top-[4.8rem] z-50 grid h-[11.6rem] w-[32rem] grid-cols-6 grid-rows-2 gap-x-[2rem] gap-y-[1.2rem] rounded-[8px] bg-gray-bg-04 px-[2rem] py-[2.2rem]"
+			className={`absolute left-0 z-50 grid h-[11.6rem] w-[32rem] grid-cols-6 grid-rows-2 gap-x-[2rem] gap-y-[1.2rem] rounded-[8px] bg-gray-bg-04 px-[2rem] py-[2.2rem] ${className}`}
 		>
 			{children}
 		</div>
@@ -25,11 +26,15 @@ const ColorPaletteRoot = forwardRef<HTMLDivElement, ColorPaletteRootProps>(funct
 
 interface ColorPaletteColorButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	hashColor: keyof typeof COLOR_PALETTE_MAP;
+	isSelected?: boolean;
 }
 
-const ColorPaletteColorButton = ({ hashColor, ...props }: ColorPaletteColorButtonProps) => {
+const ColorPaletteColorButton = ({ hashColor, isSelected, ...props }: ColorPaletteColorButtonProps) => {
 	return (
-		<button {...props} className={`h-[3rem] w-[3rem] flex-shrink-0 rounded-full ${COLOR_PALETTE_MAP[hashColor]}`} />
+		<button
+			{...props}
+			className={`h-[3rem] w-[3rem] flex-shrink-0 rounded-full ${isSelected && 'border-[2px] border-white'} ${COLOR_PALETTE_MAP[hashColor]}`}
+		/>
 	);
 };
 
