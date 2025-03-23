493c7ef4b292868807d5aeaa45f1cfbc63d43d46
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-03-23 21:25:20 +0900

    feat: sse 관련 상수 삭제 (#292)

--- src/shared/apisV2/SSE/SSE.endpoint.ts
+++
@@ -1,4 +0,0 @@
-export const SSE_ENDPOINT = {
-	GET_SSE_CONNECTION: 'api/v2/sse/connect',
-	GET_SSE_REFRESH: `api/v2/sse/refresh`,
-};
--- src/shared/apisV2/SSE/useSSEEvent.ts
+++
@@ -1,8 +0,0 @@
-import { useAtom } from 'jotai';
-
-import { sseEventAtom } from '@/shared/stores/atoms/SSEAtoms';
-
-export const useSSEEvent = () => {
-	const [event] = useAtom(sseEventAtom);
-	return event;
-};
