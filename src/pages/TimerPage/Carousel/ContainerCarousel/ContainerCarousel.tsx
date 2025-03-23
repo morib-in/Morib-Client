18da9ab93bccdf23e38dfcec8bcde0e3014ec369
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-03-24 03:27:22 +0900

    feat: 바뀐 친구 응답 api 타입에 맞추어 캐러셀 친구 로직 수정 (#292)

--- src/pages/TimerPage/Carousel/ContainerCarousel/ContainerCarousel.tsx
+++ src/pages/TimerPage/Carousel/ContainerCarousel/ContainerCarousel.tsx
@@ -1,6 +1,9 @@
 import { formatSeconds } from '@/shared/utils/time';
 
-import ClockIcon from '@/shared/assets/svgs/icon_clock.svg?react';
+import DefaultProfileIcon from '@/shared/assets/svgs/default_profile.svg';
+import ActivatedClockIcon from '@/shared/assets/svgs/icon_clock.svg?react';
+import DeactivatedClockIcon from '@/shared/assets/svgs/timer/ic_deactivated_clock.svg?react';
+import OnlineIcon from '@/shared/assets/svgs/timer/ic_online.svg?react';
 
 import useCarouselTimer from './hooks/useCarouselTimer';
 
@@ -10,25 +13,38 @@ interface ContainerCarouselProps {
 	time: number;
 	categoryname: string;
 	isPlaying: boolean;
+	isOnline: boolean;
 }
 
 const ContainerCarousel = ({ image, name, time, categoryname, isPlaying }: ContainerCarouselProps) => {
 	const timer = useCarouselTimer({ isPlaying, previousTime: time });
 	const formattedTime = formatSeconds(timer);
 
+	const ClockIcon = isPlaying ? ActivatedClockIcon : DeactivatedClockIcon;
+
 	return (
 		<>
-			{time === 0 ? null : (
-				<div className="flex h-[15rem] w-[9.8rem] flex-col items-center justify-center px-[0.8rem] py-[0.5rem]">
-					<img src={image} alt="유저 프로필" className="mb-[1rem] h-[7.4rem] w-[7.4rem]" />
-					<div className="flex justify-center gap-[0.2rem]">
-						<ClockIcon />
-						<span className={`text-mint-02 detail-reg-14`}>{formattedTime}</span>
-					</div>
-					<span className="text-white detail-semibold-14">{name}</span>
-					<span className="text-gray-04 detail-reg-12">{categoryname}</span>
+			<div className="relative flex h-[15rem] w-[9.8rem] flex-shrink-0 flex-col items-center justify-center px-[0.8rem] py-[0.5rem]">
+				<span className="relative mb-[2.9rem] h-[7.4rem] w-[7.4rem]">
+					<img
+						src={image}
+						alt="유저 프로필"
+						onError={(e) => {
+							e.currentTarget.onerror = null;
+							e.currentTarget.src = DefaultProfileIcon;
+						}}
+						className="h-[7.4rem] w-[7.4rem] rounded-full"
+					/>
+					<OnlineIcon className="absolute bottom-[0.4rem] right-[0.6rem] flex-shrink-0 rounded-full border-[0.2rem] border-gray-bg-01" />
+				</span>
+
+				<div className="absolute top-[8.4rem] flex items-center gap-[0.4rem]">
+					<ClockIcon />
+					<span className={`detail-reg-14 ${isPlaying ? 'text-mint-02' : 'text-gray-03'}`}>{formattedTime}</span>
 				</div>
-			)}
+				<span className="text-white detail-semibold-14">{name}</span>
+				<span className="text-gray-04 detail-reg-12">{categoryname}</span>
+			</div>
 		</>
 	);
 };
