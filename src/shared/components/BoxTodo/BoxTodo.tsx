3da777878c9b84b1c01554a5f0c76d833bfc1593
hanseo <yds06206@gmail.com>
2025-04-04 14:32:25 +0900

    remove: 필요없는 코드 삭제

--- src/shared/components/BoxTodo/BoxTodo.tsx
+++ src/shared/components/BoxTodo/BoxTodo.tsx
@@ -161,7 +161,7 @@ const BoxTodo = ({
 				<div className="ml-[0.8rem] mt-[0.7rem] flex flex-col gap-[0.2rem]">
 					<button
 						className={`flex items-center gap-[0.6rem] ${!addingComplete ? 'pointer-events-none' : ''}`}
-						onClick={!addingComplete && !isSelected ? handleCalendarToggle : undefined}
+						onClick={handleCalendarToggle}
 					>
 						<ButtonCalendarIcon />
 						<p className="mt-[0.3rem] text-gray-04 detail-reg-12">{duration}</p>
