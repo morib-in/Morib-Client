d5c583ee11bee4cc2abbe1d943449a996a73e337
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-04-12 00:58:07 +0900

    fix: 캘린더 클릭 영역 수정 및 hover 적용 (#300)

--- src/shared/components/BoxTodo/BoxTodo.tsx
+++ src/shared/components/BoxTodo/BoxTodo.tsx
@@ -160,7 +160,7 @@ const BoxTodo = ({
 				</div>
 				<div className="ml-[0.8rem] mt-[0.7rem] flex flex-col gap-[0.2rem]">
 					<button
-						className={`flex items-center gap-[0.6rem] ${!clickable ? '' : 'pointer-events-none'}`}
+						className={`flex w-[14.5rem] items-center gap-[0.6rem] rounded-[0.3rem] hover:bg-gray-bg-03 ${!clickable ? '' : 'pointer-events-none'}`}
 						onClick={handleCalendarToggle}
 					>
 						<ButtonCalendarIcon />
