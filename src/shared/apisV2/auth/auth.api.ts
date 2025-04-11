4af7e82d340b8e00b5b09ffee9e0443fb9e1efc7
hanseo <yds06206@gmail.com>
2025-04-12 02:49:13 +0900

    fix: post 빈객체 매개변수 전달

--- src/shared/apisV2/auth/auth.api.ts
+++ src/shared/apisV2/auth/auth.api.ts
@@ -14,11 +14,15 @@ const AUTH_ENDPOINT = {
 export const patchReissueToken = async (): Promise<reissueRes> => {
 	const accessToken = getAccessToken();
 
-	const { data } = await axios.post(AUTH_ENDPOINT.POST_REISSUE_TOKEN, {
-		headers: {
-			Authorization: `Bearer ${accessToken}`,
+	const { data } = await axios.post(
+		AUTH_ENDPOINT.POST_REISSUE_TOKEN,
+		{},
+		{
+			headers: {
+				Authorization: `Bearer ${accessToken}`,
+			},
 		},
-	});
+	);
 
 	return data;
 };
