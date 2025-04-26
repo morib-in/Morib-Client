9c9f4cec3b33cafa65da50180d533c655b7e06db
10tentacion <ddww1029wwdd@gmail.com>
2025-04-26 18:44:09 +0900

    feat: 온보딩 완료 여부 flag 명 변경 (#308)

--- src/pages/RedirectPage/RedirectPage.tsx
+++ src/pages/RedirectPage/RedirectPage.tsx
@@ -12,12 +12,12 @@ const RedirectPage = () => {
 	useEffect(() => {
 		const params = new URLSearchParams(search);
 		const accessToken = params.get('accessToken');
-		const isSignUp = params.get('isSignUp');
+		const 온보딩완료여부 = params.get('isOnboardingComplete');
 
 		if (accessToken) {
 			setAccessToken(accessToken);
-			if (isSignUp === 'true') {
-				localStorage.setItem('isSignUp', isSignUp);
+			if (온보딩완료여부 === 'true') {
+				localStorage.setItem('isOnboardingComplete', 온보딩완료여부);
 				navigate(`${ROUTES_CONFIG.onboarding.path}?step=start`, { replace: true });
 			} else {
 				navigate(`${ROUTES_CONFIG.home.path}`, { replace: true });
