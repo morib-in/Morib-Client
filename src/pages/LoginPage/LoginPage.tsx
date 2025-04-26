82d68f6e0dba6aa81372426c64b05260b40eebbd
10tentacion <ddww1029wwdd@gmail.com>
2025-04-26 18:44:38 +0900

    chore: 불필요 import 제거

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,13 +1,7 @@
-import { useEffect } from 'react';
 import Lottie from 'react-lottie';
-import { useNavigate } from 'react-router-dom';
-
-import { getAccessToken } from '@/shared/utils/auth';
 
 import LottieData from '@/shared/assets/lotties/morib_logo_motion.json';
 import GoogleLoginIcon from '@/shared/assets/svgs/google_login.svg?react';
-
-import { ROUTES_CONFIG } from '@/router/routesConfig';
 
 import { useLottieAnimation } from '@/pages/LoginPage/hooks/useLottieAnimation';
 
