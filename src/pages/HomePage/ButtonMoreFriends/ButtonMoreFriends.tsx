63215b488ba2353de724efb954098a790117adc1
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-04-30 21:47:21 +0900

    refactor: ButtonMoreFriends 내부 요소 위치 수정 (#308)

--- src/pages/HomePage/ButtonMoreFriends/ButtonMoreFriends.tsx
+++ src/pages/HomePage/ButtonMoreFriends/ButtonMoreFriends.tsx
@@ -9,8 +9,8 @@ interface ButtonMoreFriendsProps extends ButtonHTMLAttributes<HTMLButtonElement>
 const ButtonMoreFriends = ({ friendsCount, ...props }: ButtonMoreFriendsProps) => {
 	return (
 		<button className="relative mb-[2rem] h-[5.4rem]" {...props}>
-			<ButtonMoreFriendIcon className="absolute top-0 h-[5rem] w-[5rem] rounded-full hover:bg-gray-bg-04 2xl:h-[6rem] 2xl:w-[6rem]" />
-			<p className="absolute left-[1.2rem] top-[1.7rem] z-10 text-gray-04 body-med-16 2xl:left-[1.4rem] 2xl:top-[2.1rem] 2xl:subhead-med-18">
+			<ButtonMoreFriendIcon className="absolute top-[-0.3rem] h-[5rem] w-[5rem] rounded-full 2xl:h-[6rem] 2xl:w-[6rem]" />
+			<p className="absolute left-[1.5rem] top-[1.2rem] z-10 text-gray-04 body-med-16 2xl:left-[1.9rem] 2xl:top-[1.8rem] 2xl:subhead-med-18">
 				+{friendsCount}
 			</p>
 		</button>
