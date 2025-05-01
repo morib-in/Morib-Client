e01eb60f6fecc289c0257f23987a435a99065d18
ivoryeee <psa109@sookmyung.ac.kr>
2025-05-02 00:45:31 +0900

    style: FaviconImage 필요없는 부분 삭제 (#308)

--- src/shared/components/FaviconImage/FaviconImage.tsx
+++ src/shared/components/FaviconImage/FaviconImage.tsx
@@ -2,13 +2,12 @@ import { ImgHTMLAttributes } from 'react';
 
 import LogoPath from '@/shared/assets/svgs/logo_icon.svg';
 
-export const FaviconImage = ({ src, className = '', alt, ...rest }: ImgHTMLAttributes<HTMLImageElement>) => {
+export const FaviconImage = ({ src, className, ...rest }: ImgHTMLAttributes<HTMLImageElement>) => {
 	return (
 		<img
 			{...rest}
-			src={src}
+			src={src || LogoPath}
 			className={`h-[2rem] w-[2rem] ${className}`}
-			alt={alt}
 			onError={(e) => {
 				e.currentTarget.src = LogoPath;
 				e.currentTarget.alt = '모립 로고 아이콘';
