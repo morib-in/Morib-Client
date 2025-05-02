d6e9761d65756417bfe726b58481dea55ab23c71
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-05-02 18:12:06 +0900

    style: RegisterAllowedService 스타일 수정 (#308)

--- src/pages/HomePage/ModalContentsAlert/RegisterAllowedService/RegisterAllowedService.tsx
+++ src/pages/HomePage/ModalContentsAlert/RegisterAllowedService/RegisterAllowedService.tsx
@@ -3,7 +3,7 @@ import { AlertModalProps } from '../types/index';
 
 const RegisterAllowedService = ({ onCloseModal, onConfirm }: AlertModalProps) => (
 	<div className="flex h-[20rem] w-[47.2rem] flex-col gap-[3rem] rounded-[8px] bg-gray-bg-04 p-[3rem]">
-		<div className="flex flex-col items-center gap-[0.1rem]">
+		<div className="flex flex-col items-center gap-[1rem]">
 			<p className="text-white subhead-bold-22">허용 서비스 세트를 먼저 등록해주세요.</p>
 			<p className="text-gray-05 subhead-med-18">허용 서비스를 등록하러 갈까요?</p>
 		</div>
@@ -12,7 +12,7 @@ const RegisterAllowedService = ({ onCloseModal, onConfirm }: AlertModalProps) =>
 				등록하기
 			</ButtonAlert>
 			<ButtonAlert variant="primary" onClick={onCloseModal}>
-				취소하기
+				나중에 하기
 			</ButtonAlert>
 		</div>
 	</div>
