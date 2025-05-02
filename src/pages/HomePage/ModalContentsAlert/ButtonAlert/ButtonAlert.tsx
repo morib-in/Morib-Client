a3b2ef90754f6d78305ed255bfae32dacab5b715
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-05-02 17:01:03 +0900

    feat: 허용서비스세트 등록 요청 alert 컴포넌트 구현 (#308)

--- src/pages/HomePage/ModalContentsAlert/ButtonAlert/ButtonAlert.tsx
+++ src/pages/HomePage/ModalContentsAlert/ButtonAlert/ButtonAlert.tsx
@@ -1,19 +1,22 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react';
 
 interface ButtonAlertProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-	variant?: 'primary' | 'danger';
+	variant?: 'primary' | 'danger' | 'mint';
 	children: ReactNode;
 	className?: string;
 }
 
 const ButtonAlert = ({ children, variant = 'primary', className = '', ...props }: ButtonAlertProps) => {
-	const primaryStyle = 'bg-gray-bg-06 hover:bg-gray-bg-04 active:bg-gray-bg-05';
-	const dangerStyle = 'bg-error-01 hover:bg-error-03 active:bg-error-03 active:text-gray-04';
-	const buttonStyle = variant === 'primary' ? primaryStyle : dangerStyle;
+	const primaryStyle = 'bg-gray-bg-06 hover:bg-gray-bg-04 text-white active:bg-gray-bg-05';
+	const dangerStyle = 'bg-error-01 hover:bg-error-03 active:bg-error-03 text-white active:text-gray-04';
+	const mintStyle = 'bg-mint-02 hover:bg-mint-01 active:bg-mint-03 text-black active:text-gray-01';
+
+	const buttonStyle = variant === 'primary' ? primaryStyle : variant === 'danger' ? dangerStyle : mintStyle;
+
 	return (
 		<button
 			{...props}
-			className={`w-full rounded-[5px] px-[4.8rem] py-[1rem] text-center text-white subhead-semibold-18 ${buttonStyle} ${className}`}
+			className={`w-full rounded-[5px] px-[4.8rem] py-[1rem] text-center subhead-semibold-18 ${buttonStyle} ${className}`}
 		>
 			{children}
 		</button>
