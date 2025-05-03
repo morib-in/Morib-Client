10229f4944594bffff9892acab870c7c0f96112b
10tacion <127329855+10tacion@users.noreply.github.com>
2025-05-03 16:50:05 +0900

    feat: refresh token 쿼리 스트링 파싱 방식으로 변경 (#308) (#316)

--- src/pages/RedirectPage/RedirectPage.tsx
+++ src/pages/RedirectPage/RedirectPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { setAccessToken } from '@/shared/utils/auth';
+import { setAccessToken, setRefreshToken } from '@/shared/utils/auth';
 
 import { ROUTES_CONFIG } from '@/router/routesConfig';
 
@@ -12,16 +12,21 @@ const RedirectPage = () => {
 	useEffect(() => {
 		const params = new URLSearchParams(search);
 		const accessToken = params.get('accessToken');
+		const refreshToken = params.get('refreshToken');
 		const 온보딩완료여부 = params.get('isOnboardingComplete');
 
-		if (accessToken) {
+		if (accessToken && refreshToken) {
 			setAccessToken(accessToken);
+			setRefreshToken(refreshToken);
+
 			if (온보딩완료여부 === 'true') {
 				localStorage.setItem('isOnboardingComplete', 온보딩완료여부);
 				navigate(`${ROUTES_CONFIG.onboarding.path}?step=start`, { replace: true });
 			} else {
 				navigate(`${ROUTES_CONFIG.home.path}`, { replace: true });
 			}
+		} else {
+			navigate(`${ROUTES_CONFIG.login.path}`, { replace: true });
 		}
 	}, [navigate, search]);
 
--- src/shared/apisV2/client.ts
+++ src/shared/apisV2/client.ts
@@ -40,6 +40,7 @@ const addAuthInterceptor = (axiosClient: AxiosInstance) => {
 				prevRequest.sent = true;
 				// 401 에러가 떴을 때 토큰 재발급
 				try {
+					// NOTE: 추후에 로컬스토리지에 저장된 refreshToken을 사용하게끔 고치는 것이 필요할수있음
 					const { data } = await postReissueToken();
 					setAccessToken(data.accessToken);
 					return axiosClient(prevRequest);
