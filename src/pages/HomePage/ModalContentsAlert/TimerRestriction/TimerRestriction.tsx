695ab59cccb3b7ca355b67c12547973e45f48215
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-05-03 15:24:32 +0900

    style: TimerRestriction 문구 수정 (#308)

--- src/pages/HomePage/ModalContentsAlert/TimerRestriction/TimerRestriction.tsx
+++ src/pages/HomePage/ModalContentsAlert/TimerRestriction/TimerRestriction.tsx
@@ -3,7 +3,10 @@ import { AlertModalProps } from '../types/index';
 
 const TimerRestriction = ({ onConfirm }: AlertModalProps) => (
 	<div className="flex h-[19.8rem] w-[47.2rem] flex-col gap-[3rem] rounded-[8px] bg-gray-bg-04 p-[3rem]">
-		<p className="text-center text-gray-05 subhead-bold-22">오늘 날짜에 해당하는 할 일만 타이머를 실행할 수 있어요.</p>
+		<div className="flex flex-col">
+			<p className="text-center text-gray-05 subhead-bold-22">오늘 날짜에 해당하는 할 일만</p>
+			<p className="text-center text-gray-05 subhead-bold-22">추가할 수 있어요.</p>
+		</div>
 		<ButtonAlert variant="primary" onClick={onConfirm}>
 			확인
 		</ButtonAlert>
