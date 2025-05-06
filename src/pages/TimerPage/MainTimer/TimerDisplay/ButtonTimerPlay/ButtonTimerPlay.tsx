fa5179c57504e4710c135d9f8b931a9d7da95d9f
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-05-07 05:32:18 +0900

    refactor: 타이머페이지 완료된 할 일 카드인 경우 타이머 실행 막기 (#308)

--- src/pages/TimerPage/MainTimer/TimerDisplay/ButtonTimerPlay/ButtonTimerPlay.tsx
+++ src/pages/TimerPage/MainTimer/TimerDisplay/ButtonTimerPlay/ButtonTimerPlay.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from 'react';
+
 import PauseIcon from '@/shared/assets/svgs/defaultpause.svg?react';
 import PlayIcon from '@/shared/assets/svgs/defaultplay.svg?react';
 import HoverPauseIcon from '@/shared/assets/svgs/hoverpause.svg?react';
@@ -6,14 +8,27 @@ import HoverPlayIcon from '@/shared/assets/svgs/hoverplay.svg?react';
 interface ButtonTimerPlayProps {
 	onClick: () => void;
 	isPlaying: boolean;
+	disabled?: boolean;
 }
 
-const ButtonTimerPlay = ({ onClick, isPlaying }: ButtonTimerPlayProps) => {
+const ButtonTimerPlay = ({ onClick, isPlaying, disabled = false }: ButtonTimerPlayProps) => {
 	const IconComponent = isPlaying ? PauseIcon : PlayIcon;
 	const HoverIconComponent = isPlaying ? HoverPauseIcon : HoverPlayIcon;
 
+	const handleClick = (e: MouseEvent) => {
+		if (disabled) {
+			e.preventDefault();
+			return;
+		}
+		onClick();
+	};
+
 	return (
-		<button onClick={onClick} className="group relative">
+		<button
+			onClick={handleClick}
+			className={`group relative ${disabled ? 'cursor-not-allowed' : ''}`}
+			disabled={disabled}
+		>
 			<IconComponent className="block group-hover:hidden" />
 			<HoverIconComponent className="hidden group-hover:block" />
 		</button>
