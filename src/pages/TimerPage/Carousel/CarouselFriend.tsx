905747dcbfb0c2f08827c9b100a4ea33dee3073f
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-05-07 17:38:05 +0900

    style: 타이머페이지 친구리스트 캐러셀 스타일 조정 (#308)

--- src/pages/TimerPage/Carousel/CarouselFriend.tsx
+++ src/pages/TimerPage/Carousel/CarouselFriend.tsx
@@ -41,9 +41,9 @@ const CarouselFriend = memo(function CarouselFriend({
 	const ClockIcon = isPlaying ? ActivatedClockIcon : DeactivatedClockIcon;
 
 	return (
-		<div className="relative flex h-[15rem] w-[9.8rem] flex-shrink-0 flex-col items-center justify-center px-[0.8rem] py-[0.5rem]">
+		<div className="flex h-[15rem] w-[9.8rem] flex-shrink-0 flex-col items-center justify-center px-[0.8rem] py-[0.5rem]">
 			{/* 프로필 이미지 영역 */}
-			<span className="relative mb-[2.9rem] h-[7.4rem] w-[7.4rem]">
+			<span className="relative h-[7.4rem] w-[7.4rem]">
 				<img
 					src={image}
 					alt={`${name}의 프로필`}
@@ -59,9 +59,11 @@ const CarouselFriend = memo(function CarouselFriend({
 			</span>
 
 			{/* 타이머 표시 영역 */}
-			<div className="absolute top-[8.4rem] flex items-center gap-[0.4rem]">
+			<div className="mb-[0.2rem] mt-[1rem] flex items-center gap-[0.2rem]">
 				<ClockIcon />
-				<span className={`detail-reg-14 ${isPlaying ? 'text-mint-02' : 'text-gray-03'}`}>{formattedTime}</span>
+				<span className={`pt-[0.1rem] detail-reg-14 ${isPlaying ? 'text-mint-02' : 'text-gray-03'}`}>
+					{formattedTime}
+				</span>
 			</div>
 
 			{/* 이름 및 카테고리 표시 */}
