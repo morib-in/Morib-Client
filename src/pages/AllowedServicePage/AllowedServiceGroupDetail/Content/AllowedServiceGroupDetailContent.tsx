2c829d4f971fc2d3e96db0aea8ede3b45204a17a
hanseo <yds06206@gmail.com>
2025-05-08 16:54:45 +0900

    feat: 드롭다운 zIndex 변경

--- src/pages/AllowedServicePage/AllowedServiceGroupDetail/Content/AllowedServiceGroupDetailContent.tsx
+++ src/pages/AllowedServicePage/AllowedServiceGroupDetail/Content/AllowedServiceGroupDetailContent.tsx
@@ -83,7 +83,8 @@ export const AllowedServiceGroupDetailContentTableRow = ({
 							<MeatBallDefaultIcon className="cursor-pointer hover:rounded-full hover:bg-gray-bg-05" />
 						</Dropdown.Trigger>
 						<Dropdown.Content boxShadow="shadow-none" className="absolute right-0 top-[2.4rem] w-[12.4rem]">
-							<Dropdown.Item label="리스트 삭제" textColor="red" onClick={onDeleteAllowedSite} />
+							<Dropdown.Item label="상위 도메인 허용" />
+							<Dropdown.Item label="허용 사이트 삭제" textColor="red" onClick={onDeleteAllowedSite} />
 						</Dropdown.Content>
 					</Dropdown>
 				</div>
