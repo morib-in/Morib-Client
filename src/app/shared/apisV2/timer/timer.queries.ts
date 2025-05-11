31bf1b01bd78252cec5495a6174035d04c417090
10tentacion <ddww1029wwdd@gmail.com>
2025-05-12 00:10:55 +0900

    feat: throwOnError를 통해서 에러 객체를 감지하게 설정

--- src/app/shared/apisV2/timer/timer.queries.ts
+++ src/app/shared/apisV2/timer/timer.queries.ts
@@ -54,6 +54,7 @@ export const useGetSelectedTimerTask = (
 	return useQuery<GetSelectedTimerTaskRes, Error>({
 		queryKey: timerKeys.selectedTimerTask({ targetDate }),
 		queryFn: () => getSelectedTimerTask({ targetDate }),
+		throwOnError: false,
 		staleTime: 5000, // 5초 동안 캐시 데이터 사용
 		...options,
 	});
