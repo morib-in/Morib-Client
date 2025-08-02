e2ec7afe844dfcb0aad3a74c95eae5cdeb968b45
10tentacion <ddww1029wwdd@gmail.com>
2025-08-02 17:03:32 +0900

    feat: 사용되지 않는 리다이렉션 함수/훅 삭제

--- src/app/shared/utils/auth.ts
+++ src/app/shared/utils/auth.ts
@@ -1,5 +1,3 @@
-import { ROUTES_CONFIG } from '@/router/routesConfig';
-
 export const getAccessToken = () => {
 	const accessToken = localStorage.getItem('accessToken');
 	return accessToken;
@@ -7,21 +5,6 @@ export const getAccessToken = () => {
 
 export const setAccessToken = (accessToken: string) => {
 	localStorage.setItem('accessToken', accessToken);
-};
-
-export const reloginWithoutLogout = () => {
-	localStorage.removeItem('accessToken');
-	localStorage.removeItem('refreshToken');
-	localStorage.removeItem('isOnboardingCompleted');
-
-	// electron 환경인지 확인
-	if (window.electron?.auth) {
-		// electron IPC 통신을 통해 메인 프로세스에 메시지 전송
-		window.electron.auth.relogin();
-	} else {
-		// 일반 브라우저 환경인 경우 기존 방식대로 처리
-		location.href = ROUTES_CONFIG.login.path;
-	}
 };
 
 export const getRefreshToken = () => {
